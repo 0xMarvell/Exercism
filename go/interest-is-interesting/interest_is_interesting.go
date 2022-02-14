65f518c52d86b811f3c02071738f8f45c97e0517
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-02-14 22:03:24 +0100

    completed final part of interest exercise (go)

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -24,10 +24,17 @@ func Interest(balance float64) float64 {
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	//panic("Please implement the AnnualBalanceUpdate function")
+	return Interest(balance) + balance
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	//panic("Please implement the YearsBeforeDesiredBalance function")
+	var target int
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		target += 1
+	}
+	return target
 }
