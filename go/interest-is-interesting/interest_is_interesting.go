98b5e455f931c873f0c3ad2a4b3db02ef3634686
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-02-14 21:34:16 +0100

    completed 1st part of interest exercise (go)

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -2,7 +2,17 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	//panic("Please implement the InterestRate function")
+	switch {
+	case balance >= 0 && balance < 1000:
+		return float32(0.5)
+	case balance >= 1000 && balance < 5000:
+		return float32(1.621)
+	case balance >= 5000:
+		return float32(2.475)
+	default:
+		return float32(3.213)
+	}
 }
 
 // Interest calculates the interest for the provided balance.
