74519080e04aca7a38b7d3b7a59a9ce5f22b679d
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-02-15 20:16:13 +0100

    completed 1st part of lasagna-master (go)

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,46 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+const (
+	defautlPreparationTimePerLayer = 2
+	noodleQtyPerLayer              = 50
+	sauceQtyPerLayer               = 0.2
+	defaultServingInRecipe         = 2
+)
 
-// TODO: define the 'Quantities()' function
+// PreparationTime estimates the total preparation time based on the number of layers
+func PreparationTime(layers []string, time int) int {
+	if time <= 0 {
+		time = defautlPreparationTimePerLayer
+	}
+	return len(layers) * time
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+// Quantities determines the quantity of noodles and sauce needed to make your meal
+// func Quantities(layers []string) (int, float64) {
+// 	noodleLayers := 0
+// 	sauceLayers := 0
+// 	for _, l := range layers {
+// 		switch l {
+// 		case "noodles":
+// 			noodleLayers++
+// 		case "sauce":
+// 			sauceLayers++
+// 		}
+// 	}
+// 	return noodleLayers * noodleQtyPerLayer, float64(sauceLayers) * sauceQtyPerLayer
+// }
 
-// TODO: define the 'ScaleRecipe()' function
+// AddSecretIngredient adds the secret ingredient to your recipe
+// func AddSecretIngredient(friendsList, myList []string) {
+// 	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+// }
+
+// ScaleRecipe gives the amounts needed for the desired number of portions
+// func ScaleRecipe(amounts []float64, quantities int) []float64 {
+// 	var scaledAmounts []float64
+
+// 	for _, recipeQty := range amounts {
+// 		scaledAmounts = append(scaledAmounts, recipeQty/defaultServingInRecipe*float64(quantities))
+// 	}
+// 	return scaledAmounts
+// }
