66db2b5cccabc9b5f37d858411a303f608cd465a
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-04 20:30:20 +0100

    completed 1st part of logs logs logs (go)

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,8 +1,28 @@
 package logs
 
+import "fmt"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	//panic("Please implement the Application() function")
+	var application string
+
+	for _, val := range log {
+		appUnicode := fmt.Sprintf("%U", val)
+		if appUnicode == "U+2757" {
+			application = "recommendation"
+			break
+		} else if appUnicode == "U+1F50D" {
+			application = "search"
+			break
+		} else if appUnicode == "U+2600" {
+			application = "weather"
+			break
+		} else {
+			application = "default"
+		}
+	}
+	return application
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
