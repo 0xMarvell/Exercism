fbde7470d5dcf4b6ef4e6f2be4f9f6f24862cec5
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-09 14:53:03 +0100

    completed 1st part of elons toys (go)

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,6 +1,12 @@
 package elon
 
 // TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.battery -= c.batteryDrain
+		c.distance += c.speed
+	}
+}
 
 // TODO: define the 'DisplayDistance() string' method
 
