5f87eedd59cdf0bfa70d8f7a988eeccd1ab2cd9d
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-16 16:05:55 +0100

    added comments for clearer understanding (two-fer)

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,12 +1,12 @@
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer — Two-fer or 2-fer is short for two for one. One for you and one for me.
 package twofer
 
 import (
 	"fmt"
 )
 
-// ShareWith should have a comment documenting it.
+// ShareWith takes a name as argument and returns a string with this name. However, if the name
+// is missing, the function returns the string with the string 'you' in place of the name variable.
 func ShareWith(name string) string {
 	if name != "" {
 		return fmt.Sprintf("One for %s, one for me.", name)
