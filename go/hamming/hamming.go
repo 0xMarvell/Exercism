5c4ae470155b473b4e9afb8c72afdb2fa0ac9d8d
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-18 17:15:46 +0100

    Completed hamming exercise (go)

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	//panic("Implement the Distance function")
+	if len(a) == len(b) {
+		hammingDistance := 0
+		for i := range a {
+			if a[i] != b[i] {
+				hammingDistance++
+			}
+		}
+		return hammingDistance, nil
+	} else {
+		return 0, errors.New("The DNA strands match. One may be longer than the other.")
+	}
 }
