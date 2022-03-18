deae9eca07c404f15946b481756c494855b133d8
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-18 17:40:50 +0100

    completed isogram exercise(go)

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,22 @@
 package isogram
 
+import (
+	"strings"
+	"unicode"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	//panic("Please implement the IsIsogram function")
+	lowercase := strings.ToLower(word)
+	read_word := make(map[rune]bool)
+	for _, v := range lowercase {
+		if !unicode.IsLetter(v) {
+			continue
+		}
+		if read_word[v] {
+			return false
+		}
+		read_word[v] = true
+	}
+	return true
 }
