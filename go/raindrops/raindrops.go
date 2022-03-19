d8a92f4c129b120ea67af7b49586a4568d3e732e
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-19 17:18:13 +0100

    completed raindrops exercise (go)

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,26 @@
 package raindrops
 
+import (
+	"strconv"
+)
+
+// Convert Function converts an integer to either a "Pling", "Plang" or "Plong" sound
+// depicting the sound of rainfrops
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var drops string
+
+	if number%3 == 0 {
+		drops += "Pling"
+	}
+	if number%5 == 0 {
+		drops += "Plang"
+	}
+	if number%7 == 0 {
+		drops += "Plong"
+	}
+	if drops == "" {
+		drops = strconv.Itoa(number)
+	}
+	return drops
+
 }
