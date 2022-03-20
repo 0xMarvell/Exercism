e0fe15ffa0582328ef87c350954230e6bb5d0d4d
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-20 20:43:30 +0100

    completed 1st part of grains(go)

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,7 +1,13 @@
 package grains
 
+import "errors"
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	//panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, errors.New("square value should be greater than 0 and less or equal 64")
+	}
+	return 1 << (number - 1), nil
 }
 
 func Total() uint64 {
