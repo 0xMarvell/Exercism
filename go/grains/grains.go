e665e3ef7af8b2572aa7f56b7827698ceb875ae0
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-20 20:48:09 +0100

    deleted panic statements

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,7 +3,6 @@ package grains
 import "errors"
 
 func Square(number int) (uint64, error) {
-	//panic("Please implement the Square function")
 	if number < 1 || number > 64 {
 		return 0, errors.New("square value should be greater than 0 and less or equal 64")
 	}
@@ -11,6 +10,5 @@ func Square(number int) (uint64, error) {
 }
 
 func Total() uint64 {
-	//panic("Please implement the Total function")
 	return 1<<64 - 1
 }
