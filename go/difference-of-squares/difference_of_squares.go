69435901e3cfd4af3e8aa76f8c0197d853268a33
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-20 20:30:45 +0100

    added descriptive comments for all functions

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,5 +1,6 @@
 package diffsquares
 
+// SquareOfSum calculates the square of the sum of n natural numbers
 func SquareOfSum(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -8,6 +9,7 @@ func SquareOfSum(n int) int {
 	return sum * sum
 }
 
+// SumOfSquares calculates the sum of the squares of n natural numbers
 func SumOfSquares(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -16,6 +18,7 @@ func SumOfSquares(n int) int {
 	return sum
 }
 
+// Difference calculates the difference between the values of SquareOfSum(n) and SumOfSquares(n)
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
