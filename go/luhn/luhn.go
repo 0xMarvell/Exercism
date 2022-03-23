d759c1480b7f7ff120db527b8a183e71d9667daf
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-23 21:35:14 +0100

    compleed luhn exercise (go)

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,37 @@
 package luhn
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	//panic("Please implement the Valid function")
+	id = strings.ReplaceAll(id, " ", "")
+
+	if len(id) < 2 {
+		return false
+	}
+
+	isSecond := len(id)%2 == 0
+	sum := 0
+	for _, ch := range id {
+
+		if !unicode.IsDigit(ch) {
+			return false
+		}
+
+		n := int(ch - '0')
+		// double every second digit
+		if isSecond {
+			n *= 2
+			if n > 9 {
+				n -= 9
+			}
+		}
+		sum += n
+		isSecond = !isSecond
+	}
+
+	return sum%10 == 0
 }
