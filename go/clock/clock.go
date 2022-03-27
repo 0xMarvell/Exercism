a346fe59ad2a84a56aacd694cb393a5db050f096
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-03-27 19:10:03 +0100

    deleted redundant comments

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -45,19 +45,3 @@ func parseMinutes(m int) int {
 	}
 	return m
 }
-
-// func New(h, m int) Clock {
-// 	panic("Please implement the New function")
-// }
-
-// func (c Clock) Add(m int) Clock {
-// 	panic("Please implement the Add function")
-// }
-
-// func (c Clock) Subtract(m int) Clock {
-// 	panic("Please implement the Subtract function")
-// }
-
-// func (c Clock) String() string {
-// 	panic("Please implement the String function")
-// }
