5045b018f31fe4656c11747cade50a4ba218c0c3
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-14 14:22:48 +0100

    1st attempt for isbn exercise

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,49 @@
+// Pacakge isbn checks if the inputted string is a valid ISBN number
 package isbn
 
+import (
+	"strings"
+	"unicode"
+)
+
+// IsValidISBN checks if the inputted string is a valid ISBN-1o
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	newIsbn := strings.ReplaceAll(isbn, "-", "")
+	result := 0
+
+	if len(newIsbn) < 10 {
+		return false
+	}
+
+	for i, v := range newIsbn {
+		if i < 9 && unicode.IsLetter(v) {
+			return false
+		}
+		digit := int(v - '0')
+		if v == 'X' {
+			digit = 10
+		}
+
+		result += digit * (10 - i)
+	}
+
+	return result%11 == 0
+
+	// s := strings.ReplaceAll(isbn, "-", "")
+	// if len(s) != 10 {
+	// 	return false
+	// }
+	// res := 0
+	// for i, v := range s {
+	// 	if i < 9 && unicode.IsLetter(v) {
+	// 		return false
+	// 	}
+	// dig := int(v - '0')
+	// if v == 'X' {
+	// 	dig = 10
+	// }
+	// 	res += dig * (10 - i)
+	// }
+	// return res%11 == 0
+
 }
