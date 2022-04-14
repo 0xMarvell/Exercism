648d1f36e627fef0fff0df8c4b588a18c95628ab
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-14 14:41:58 +0100

    added descriptive comments

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,7 +1,9 @@
+// Package panagram checks if given string is a panagram
 package pangram
 
 import "strings"
 
+// IsPanagram checks if the given string is a panagram
 func IsPangram(input string) bool {
 	alphabets := map[rune]bool{}
 	for _, letter := range strings.ToLower(input) {
