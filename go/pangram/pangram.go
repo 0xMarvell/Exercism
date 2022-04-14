08ccda7a096ec16e0f4333f9d8cf544346023918
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-14 14:40:36 +0100

    deleted panic statement

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -3,7 +3,6 @@ package pangram
 import "strings"
 
 func IsPangram(input string) bool {
-	//panic("Please implement the IsPangram function")
 	alphabets := map[rune]bool{}
 	for _, letter := range strings.ToLower(input) {
 		if letter >= 'a' && letter <= 'z' {
