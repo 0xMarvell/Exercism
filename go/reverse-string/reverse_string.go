0f4376da6093270f5d8b92e15b15e9c2e908b209
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-15 10:34:06 +0100

    deleted panic sttaement

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -3,7 +3,6 @@ package reverse
 // Reverse takes a string as argument and
 // returns the reversed version of the string.
 func Reverse(input string) string {
-	//panic("Please implement the Reverse function")
 	var res string
 	for _, v := range input {
 		res = string(v) + res
