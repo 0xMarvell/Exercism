258032f98f2275b6151a11007845a5c58a81ce8d
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-15 10:32:53 +0100

    added descriptive comment for reverse func

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,7 @@
 package reverse
 
+// Reverse takes a string as argument and
+// returns the reversed version of the string.
 func Reverse(input string) string {
 	//panic("Please implement the Reverse function")
 	var res string
