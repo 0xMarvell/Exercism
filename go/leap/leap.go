85b5eea66f0986b8080aa6fd41b1260874dbdba7
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-16 20:26:12 +0100

    1st attempt. all tests passing

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,6 +2,7 @@
 package leap
 
 // IsLeapYear should have a comment documenting it.
-func IsLeapYear(int) bool {
-	return false
+func IsLeapYear(year int) bool {
+	return (year%4 == 0 && year%100 != 0) || (year%4 == 0) && (year%100 == 0 && year%400 == 0)
+	// return false
 }
