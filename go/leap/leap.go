99cdf2b31753e7574741add19d61929922e65e2a
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-16 20:28:18 +0100

    added descriptive comments

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,8 +1,8 @@
-// Package leap should have a package comment that summarizes what it's about.
+// Package leap checks if the inputted year is a (Gregorian Calender) leap year or not
 package leap
 
-// IsLeapYear should have a comment documenting it.
+// IsLeapYear checks if the inputted year is
+// a (Gregorian Calender) leap year or not.
 func IsLeapYear(year int) bool {
 	return (year%4 == 0 && year%100 != 0) || (year%4 == 0) && (year%100 == 0 && year%400 == 0)
-	// return false
 }
