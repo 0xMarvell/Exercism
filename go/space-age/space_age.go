3130ee28f4c3dcb9f52a22997c941caae74ff9f1
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-16 21:51:13 +0100

    added descriptive comments

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,9 +1,11 @@
+// Package space calculates how old someone would be on any planet.
 package space
 
 type Planet string
 
 const earthInSeconds float64 = 31557600
 
+// Age calculates how old someone would be on any planet, given an age in seconds.
 func Age(seconds float64, planet Planet) float64 {
 	var planets = map[Planet]float64{
 		"Earth":   1,
