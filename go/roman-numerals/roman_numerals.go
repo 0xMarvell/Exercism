48bcbabf8faae10e6e9780294c01c7824618c322
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-17 17:59:08 +0100

    1st attempt. all tests passing

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,5 +1,33 @@
 package romannumerals
 
+import "errors"
+
+// ToRomanNumeral converts a number to it's roman numeral equivalent
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	//panic("Please implement the ToRomanNumeral function")
+	aToR := []struct {
+		regular int
+		roman   string
+	}{
+		{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"},
+		{10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"},
+		{100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"},
+		{1000, "M"},
+	}
+
+	if input <= 0 || input > 3000 {
+		return "", errors.New("out of range")
+	}
+
+	res := ""
+	for i := len(aToR) - 1; i >= 0; i-- {
+
+		for input >= aToR[i].regular {
+			input -= aToR[i].regular
+			res += aToR[i].roman
+		}
+
+	}
+
+	return res, nil
 }
