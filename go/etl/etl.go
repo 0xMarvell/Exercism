e7d18777da9041e5b24dc20591df0d3889057c96
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-22 21:56:31 +0100

    1st attempt. all tests passing

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,16 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	//panic("Please implement the Transform function")
+	scrabble := make(map[string]int)
+
+	for i, v := range in {
+		for _, letter := range v {
+			scrabble[strings.ToLower(letter)] = i
+		}
+	}
+
+	return scrabble
 }
