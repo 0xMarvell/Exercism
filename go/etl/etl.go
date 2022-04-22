9beae2a06b4060198507bd8dc343bb5e288bf570
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-22 21:58:58 +0100

    deleted panic statement

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -5,7 +5,6 @@ import "strings"
 // Transform transforms data from map into an array
 // then returns the data in new format
 func Transform(in map[int][]string) map[string]int {
-	//panic("Please implement the Transform function")
 	scrabble := make(map[string]int)
 
 	for i, v := range in {
