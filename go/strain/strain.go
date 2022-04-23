83282048787eb94b1be7ff7323ee240fdb17e4e9
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-23 20:10:20 +0100

    deleted panic statements

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -5,8 +5,8 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	//panic("Please implement the Keep function")
 	var collection Ints
+
 	for _, v := range i {
 		if filter(v) {
 			collection = append(collection, v)
@@ -16,8 +16,8 @@ func (i Ints) Keep(filter func(int) bool) Ints {
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	//panic("Please implement the Discard function")
 	var collection Ints
+
 	for _, v := range i {
 		if !filter(v) {
 			collection = append(collection, v)
@@ -27,8 +27,8 @@ func (i Ints) Discard(filter func(int) bool) Ints {
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	//panic("Please implement the Keep function")
 	var collection Lists
+
 	for _, v := range l {
 		if filter(v) {
 			collection = append(collection, v)
@@ -38,8 +38,8 @@ func (l Lists) Keep(filter func([]int) bool) Lists {
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	//panic("Please implement the Keep function")
 	var collection Strings
+
 	for _, v := range s {
 		if filter(v) {
 			collection = append(collection, v)
