92845400a4ef392420b748022f18f30320f8fb72
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-24 19:44:59 +0100

    1st attempt. all tests passing

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,22 @@
 package strand
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	//panic("Please implement the ToRNA function")
+	rna := ""
+
+	for _, nucleotide := range dna {
+		switch {
+		case nucleotide == 'G':
+			rna += "C"
+		case nucleotide == 'C':
+			rna += "G"
+		case nucleotide == 'T':
+			rna += "A"
+		case nucleotide == 'A':
+			rna += "U"
+		default:
+			return ""
+		}
+	}
+	return rna
 }
