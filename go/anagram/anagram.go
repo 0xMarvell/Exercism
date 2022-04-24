5d526ea2363a8330855afd74139233c5010464ab
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-24 15:48:45 +0100

    added descriptive comments for funcs

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,3 +1,4 @@
+// Package anagram checks if a word is an anagram or not
 package anagram
 
 import (
@@ -5,6 +6,7 @@ import (
 	"strings"
 )
 
+// Detect filters candidates selecting only anagrams of the word.
 func Detect(subject string, candidates []string) []string {
 	var anagrams []string
 
@@ -17,12 +19,15 @@ func Detect(subject string, candidates []string) []string {
 	return anagrams
 }
 
+// sortedString converts a string to a slice of string and sorts the elements
+// of the slice in ascending order the converts the sorted slice back into a string
 func sortedString(w string) string {
 	s := strings.Split(w, "")
 	sort.Strings(s)
 	return strings.Join(s, "")
 }
 
+// isAnagram checks if the candidate is an anagram of the subject
 func isAnagram(subject string, candidate string) bool {
 	return subject != candidate && sortedString(subject) == sortedString(candidate)
 }
