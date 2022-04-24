8edb0d69163f2c52f604064ef93f677bee89fcd2
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-24 15:37:28 +0100

    all tests passing

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,29 @@
 package anagram
 
+import (
+	"sort"
+	"strings"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	//panic("Please implement the Detect function")
+	var anagrams []string
+
+	for _, candidate := range candidates {
+		if isAnagram(strings.ToLower(subject), strings.ToLower(candidate)) {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+
+	return anagrams
+}
+
+func sortedString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+func isAnagram(subject string, candidate string) bool {
+	return subject != candidate && sortedString(subject) == sortedString(candidate)
 }
