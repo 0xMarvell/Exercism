3cfce544f72e4a11e18cb5a0cc84e9fcbe5b98a1
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-11 17:57:04 +0100

    2nd attempt. tests passing

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -2,11 +2,24 @@ package rotationalcipher
 
 func RotationalCipher(plain string, shiftKey int) string {
 	//panic("Please implement the RotationalCipher function")
-	var newStr string
+	newStr := make([]rune, 0)
 
 	if shiftKey == 0 || shiftKey == 26 {
 		return plain
 	}
 
-	return newStr
+	rshiftKey := rune(shiftKey)
+
+	for _, r := range plain {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			newStr = append(newStr, 'A'+(r-'A'+rshiftKey)%26)
+		case r >= 'a' && r <= 'z':
+			newStr = append(newStr, 'a'+(r-'a'+rshiftKey)%26)
+		default:
+			newStr = append(newStr, r)
+		}
+	}
+
+	return string(newStr)
 }
