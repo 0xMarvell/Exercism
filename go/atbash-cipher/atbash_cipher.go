56621ec9539a1c5c61be922abaf5a5f7a2559da3
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-31 20:29:16 +0100

    solved

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,30 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	alpha := "abcdefghijklmnopqrstuvwxyz"
+	cipher := "zyxwvutsrqponmlkjihgfedcba"
+	var temp string
+	var result string
+	s = strings.ToLower(s)
+	for _, v := range s {
+		idx := strings.Index(alpha, string(v))
+		if idx != -1 {
+			val := cipher[idx]
+			temp += string(val)
+		} else if !unicode.IsPunct(v) && !unicode.IsSpace(v) {
+			temp += string(v)
+		}
+	}
+	for i, v := range temp {
+		result += string(v)
+		if (i+1)%5 == 0 && (i+1) != len(temp) {
+			result += " "
+		}
+	}
+	return result
 }
