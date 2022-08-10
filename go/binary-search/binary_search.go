1fd534a39039a26c7f0e23c5b058e667150b5a53
0xMarvell <rokemarvellous@gmail.com>
2022-08-10 16:32:42 +0100

    solved

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,5 +1,20 @@
 package binarysearch
 
+// SearchInts implements a binary search algorithm
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	var left, right int = 0, len(list) - 1
+
+	for left <= right {
+		middle := (left + right) / 2
+
+		if list[middle] == key {
+			return middle
+		} else if list[middle] > key {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+	}
+
+	return -1
 }
