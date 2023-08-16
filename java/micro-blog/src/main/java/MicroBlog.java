e1cb3aed09e9e78d7078b1c4a8bc599466f60990
0xMarvell <rokemarvellous@gmail.com>
2023-08-16 22:04:05 +0100

    new tasks

--- java/micro-blog/src/main/java/MicroBlog.java
+++
@@ -1,5 +0,0 @@
-class MicroBlog {
-    public String truncate(String input) {
-        return input.codePointCount(0,input.length()) < 6? input: input.substring(0,input.offsetByCodePoints(0,5));
-    }
-}
--- java/reverse-string/src/main/java/ReverseString.java
+++
@@ -1,9 +0,0 @@
-class ReverseString {
-
-    String reverse(String inputString) {
-        StringBuilder sb = new StringBuilder(inputString);
-        sb.reverse();
-        return sb.toString();
-    }
-  
-}
