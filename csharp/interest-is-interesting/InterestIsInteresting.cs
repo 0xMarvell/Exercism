291d6a7e139d29f8f9740e98af941f531f36cf09
0xMarvell <rokemarvellous@gmail.com>
2023-08-31 19:30:30 +0100

    solved

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -4,21 +4,44 @@ static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        if (balance < 0)
+        {
+            return 3.213f;
+        }
+
+        if (balance >= 5000)
+        {
+            return 2.475f;
+        }
+        else if (balance < 5000 && balance >= 1000)
+        {
+            return 1.621f;
+        }
+        else
+        {
+            return 0.5f;
+        }
     }
 
     public static decimal Interest(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+        return balance * (decimal)InterestRate(balance) / 100;
     }
 
     public static decimal AnnualBalanceUpdate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        return Interest(balance) + balance;
     }
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int years = 0;
+
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
     }
 }
