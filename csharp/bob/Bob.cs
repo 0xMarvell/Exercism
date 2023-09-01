b1bea1cc45ecbd8ce6a6ee10242da397b03a621e
0xMarvell <rokemarvellous@gmail.com>
2023-09-01 12:30:24 +0100

    solved

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,43 @@
 using System;
-
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (string.IsNullOrWhiteSpace(statement))
+        {
+            return "Fine. Be that way!";
+        }
+        else if (IsYelling(statement) && statement.TrimEnd().EndsWith("?"))
+        {
+            return "Calm down, I know what I'm doing!";
+        }
+        else if (IsYelling(statement))
+        {
+            return "Whoa, chill out!";
+        }
+        else if (statement.TrimEnd().EndsWith("?"))
+        {
+            return "Sure.";
+        }
+        else
+        {
+            return "Whatever.";
+        }
+    }
+    private static bool IsYelling(string statement)
+    {
+        bool hasLetters = false;
+        foreach (char c in statement)
+        {
+            if (char.IsLetter(c))
+            {
+                hasLetters = true;
+                if (!char.IsUpper(c))
+                {
+                    return false;
+                }
+            }
+        }
+        return hasLetters;
     }
 }
