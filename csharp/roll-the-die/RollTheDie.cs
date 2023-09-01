6b09b6330a3af254d968bec34dc5f62ddf1cff79
0xMarvell <rokemarvellous@gmail.com>
2023-09-01 09:41:42 +0100

    solved

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -4,11 +4,13 @@ public class Player
 {
     public int RollDie()
     {
-        throw new NotImplementedException("Please implement the Player.RollDie() method");
+        Random rand = new();
+        return rand.Next(1,19);
     }
 
     public double GenerateSpellStrength()
     {
-        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+        Random rand = new();
+        return (double)rand.Next(100);
     }
 }
